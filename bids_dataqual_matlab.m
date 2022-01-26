f90d51afadbe0bba4fcef7565dc82ade16087a86
Dung Truong <dt.young112@gmail.com>
2022-01-26 09:43:01 -0800

    update plotting

--- bids_dataqual_matlab.m
+++ bids_dataqual_matlab.m
@@ -21,6 +21,11 @@ dir(filepath);
 %filepath = '/data/data/STUDIES/BIDS_EXPORT';
 %filepath = '/Users/arno/GoogleDrive/bids_p3_2subjects/BIDS';
 
+% clear error log file
+fid = fopen(sprintf('%s/processing-log/%s_err.txt',root_path,dsname),'w');
+fprintf(fid, '');
+fclose(fid);
+
 % read or import data
 %pop_editoptions( 'option_storedisk', 1);
 useBidsChans = { 'ds002718' 'ds003190' 'ds002578' 'ds002887' 'ds003004' 'ds002833' 'ds002691' 'ds002791' 'ds001787' 'ds003474', 'ds003645' };
@@ -46,12 +51,12 @@ percentBrainICs     = zeros(1, length(ALLEEG))*NaN;
 asrFail             = zeros(1, length(ALLEEG));
 icaFail             = zeros(1, length(ALLEEG));
 try
-    parpool;
+    %parpool;
 catch
     warning('starting parpool failed. parpool might already be created');
 end
 
-parfor iDat = 1:length(ALLEEG)
+for iDat = 1:length(ALLEEG)
 try
     EEG = ALLEEG(iDat);
     
@@ -69,11 +74,11 @@ try
         try
             dipfit_path = fileparts(which('pop_dipplot'));
         catch
-            dipfit_path = fullfile(root_path, 'eeglab','plugins','dipfit4.0');
+            dipfit_path = fullfile(root_path, 'eeglab','plugins','dipfit');
         end
         chanfile = [dipfit_path '/standard_BEM/elec/standard_1005.elc'];
         if ~exist(chanfile,'file')
-            chanfile = '/home/octave/eeglab/plugins/dipfit4.0/standard_BEM/elec/standard_1005.elc';
+            chanfile = '/home/octave/eeglab/plugins/dipfit/standard_BEM/elec/standard_1005.elc';
         end
         if isfield(EEG.chanlocs, 'theta')
             notEmpty = ~cellfun(@isempty, { EEG.chanlocs.theta });
@@ -106,11 +111,13 @@ try
                 EEGTMP = pop_clean_rawdata( EEG,'FlatlineCriterion',5,'ChannelCriterion',0.8,'LineNoiseCriterion',4,'Highpass',[0.25 0.75] ,...
                     'BurstCriterion',20,'WindowCriterion',0.25,'BurstRejection','on','Distance','Euclidian','WindowCriterionTolerances',[-Inf 7] );
                 EEG = EEGTMP;
+		
+		result_basename = EEG.filename(1:end-4); % for plots
 
 		        % save EEGPLOT
                 % ------------
                 bounds = strmatch('boundary', { EEG.event.type });
-                startLat = 1;
+                startLat = round(length(EEG.times)/2);
                 if ~isempty(bounds)
                     boundLat = [ EEG.event(bounds).latency ];
                     diffLat = diff(boundLat);
@@ -119,18 +126,23 @@ try
                         startLat = boundLat(indLat(1));
                     end
                 end
-                eegplot(EEG.data(:,startLat:startLat+EEG.srate*2), 'srate', EEG.srate, ...
-                    'winlength', 2, 'eloc_file', EEG.chanlocs, 'noui', 'on');
-                textsc(['Dataset ' EEG.filename ' (2 seconds)' ], 'title');
-                h = findall(gcf,'-property','FontName');
-                set(h,'FontName','San Serif');
-                print(gcf,'-dsvg',fullfile(EEG.filepath, [ EEG.filename(1:end-4) '_eegplot.svg' ]))
-                close
+		% redo eegplot
+		eegplot(EEG.data(:,startLat:startLat+EEG.srate*2), 'srate', EEG.srate, ...
+		    'winlength', 2, 'eloc_file', EEG.chanlocs, 'noui', 'on', 'title','');
+		print(gcf,'-dsvg','-noui',fullfile(EEG.filepath, [ result_basename '_eegplot.svg' ]))
+		close
+
+		% spectopo plot
+		figure;
+		[spec,~] = spectopo(EEG.data, 0, EEG.srate, 'freq', [6, 10, 22], 'freqrange', [1 70], 'title', '', 'chanlocs', EEG.chanlocs, 'percent', 10,'plot', 'on');
+		print(gcf,'-dsvg','-noui',fullfile(EEG.filepath,[ result_basename '_spectopo.svg' ]))
+		close
+
 
                 % run ICA and IC label
                 %if ~isempty(EEG.chanlocs) && isfield(EEG.chanlocs, 'X') && ~isempty(EEG.chanlocs(1).X)
                 try
-		            EEG = pop_reref(EEG, []);
+		    EEG = pop_reref(EEG, []);
                     amicaout = fullfile(EEG.filepath, 'amicaout');
                     if exist(amicaout, 'dir')
                         EEG = eeg_loadamica(EEG, fullfile(EEG.filepath, 'amicaout'));
@@ -143,11 +155,27 @@ try
                     EEG = pop_iclabel(EEG,'default');
                     EEG = pop_icflag(EEG,[0.6 1;NaN NaN;NaN NaN;NaN NaN;NaN NaN;NaN NaN;NaN NaN]);
 
-                    % Save IC Label image
+		% IC activations plot
+		iclocs = EEG.chanlocs;
+		% trick to set plot X axis labels to be ICs instead of EEG channels
+		for idx=1:numel(iclocs)
+		    iclocs(idx).labels = ['IC' num2str(idx)];
+		end
+                startLat = round(length(EEG.times)/2);
+		figure;
+		tmp = EEG.icaweights*EEG.icasphere*EEG.data(:,startLat:startLat+EEG.srate*2);
+		eegplot(tmp, 'srate', EEG.srate, ...
+		    'winlength', 2, 'eloc_file', iclocs, 'noui', 'on', 'title', '');
+		h = findall(gcf,'-property','FontName');
+		set(h,'FontName','San Serif');
+		print(gcf,'-dsvg',fullfile(EEG.filepath, [ result_basename '_icaact.svg' ]))
+		close
+
+                    % Save IC Label image for first 28 ICs (4x7)
                     % -------------------
-                    pop_viewprops( EEG, 0, [1:min(EEG.nbchan-1,32)], {'freqrange', [2 64]}, {}, 1, 'ICLabel' )
-                    print(gcf,'-dsvg','-noui',fullfile(EEG.filepath,[ EEG.filename(1:end-4) '_icamaps.svg' ]))
-                    close
+			pop_viewprops( EEG, 0, [1:28], {'freqrange', [2 64]}, {}, 1, 'ICLabel' );
+			print(gcf,'-dsvg','-noui',fullfile(EEG.filepath,[ result_basename '_icamaps.svg' ]))
+			close
 
                     disp(fullfile(EEG.filepath, processedEEG));
                     pop_saveset(EEG, fullfile(EEG.filepath, processedEEG));
@@ -180,9 +208,44 @@ try
     else
         % do not load binary data
         fprintf('Loading dataset %s\n', processedEEG);
-	    EEG = pop_loadset(fullfile(EEG.filepath, processedEEG));
-        %EEG = load('-mat', fullfile(EEG.filepath, processedEEG));
-        %EEG = EEG.EEG;
+	EEG = pop_loadset(fullfile(EEG.filepath, processedEEG));
+
+        result_basename = erase(EEG.filename, '_processed.set');
+
+        startLat = round(length(EEG.times)/2);
+
+	% redo eegplot
+	eegplot(EEG.data(:,startLat:startLat+EEG.srate*2), 'srate', EEG.srate, ...
+	    'winlength', 2, 'eloc_file', EEG.chanlocs, 'noui', 'on', 'title','');
+	print(gcf,'-dsvg','-noui',fullfile(EEG.filepath, [ result_basename '_eegplot.svg' ]))
+	close
+
+	% spectopo plot
+	figure;
+	[spec,~] = spectopo(EEG.data, 0, EEG.srate, 'freq', [6, 10, 22], 'freqrange', [1 70], 'title', '', 'chanlocs', EEG.chanlocs, 'percent', 10,'plot', 'on');
+        print(gcf,'-dsvg','-noui',fullfile(EEG.filepath,[ result_basename '_spectopo.svg' ]))
+	close
+
+	% IC activations plot
+	iclocs = EEG.chanlocs;
+	% trick to set plot X axis labels to be ICs instead of EEG channels
+	for idx=1:numel(iclocs)
+	    iclocs(idx).labels = ['IC' num2str(idx)];
+	end
+	figure;
+	tmp = EEG.icaweights*EEG.icasphere*EEG.data(:,startLat:startLat+EEG.srate*2);
+	eegplot(tmp, 'srate', EEG.srate, ...
+	    'winlength', 2, 'eloc_file', iclocs, 'noui', 'on', 'title', '');
+	h = findall(gcf,'-property','FontName');
+	set(h,'FontName','San Serif');
+	print(gcf,'-dsvg',fullfile(EEG.filepath, [ result_basename '_icaact.svg' ]))
+	close
+
+	% redo ICLabel plot (temp)
+        pop_viewprops( EEG, 0, [1:28], {'freqrange', [2 64]}, {}, 1, 'ICLabel' );
+        print(gcf,'-dsvg','-noui',fullfile(EEG.filepath,[ result_basename '_icamaps.svg' ]))
+        close
+
     end
     % get results
     if contains(EEG.filename, 'processed')
@@ -194,18 +257,22 @@ try
         numChans = EEG.etc.orinbchan;
         goodChans = EEG.nbchan/EEG.etc.orinbchan;
         goodData = EEG.pnts/EEG.etc.oripnts;
+	numFrames = EEG.etc.oripnts;
         goodICA = sum(EEG.reject.gcompreject)/(EEG.nbchan-1);
+	numICs = EEG.nbchan-1;
         percentBrainICs(iDat) = goodICA;
         nChans(iDat) = numChans;
         percentChanRejected(iDat) = 1-goodChans;
         percentDataRejected(iDat) = 1-goodData;
-        save_dataqual_results(EEG.filepath, result_basename, asrFail(iDat), icaFail(iDat), numChans, goodChans, goodData, goodICA)
+        save_dataqual_results(EEG.filepath, result_basename, asrFail(iDat), icaFail(iDat), numChans, goodChans, goodData, goodICA, numFrames, numICs);
     else
         numChans = EEG.etc.orinbchan;
         goodChans = nan;
         goodData = nan;
         goodICA = nan;
-        save_dataqual_results(EEG.filepath, results_basename, asrFail(iDat), icaFail(iDat), numChans, goodChans, goodData, goodICA)
+	numFrames = length(EEG.times);
+	numICs = EEG.nbchan-1;
+        save_dataqual_results(EEG.filepath, result_basename, asrFail(iDat), icaFail(iDat), numChans, goodChans, goodData, goodICA, numFrames, numICs);
     end
 catch ME
 	s=dbstack;
@@ -273,13 +340,19 @@ if fid ~= -1
     fclose(fid);
 end
 
-function save_dataqual_results(filepath, filename, asrStatus, icaStatus, numChans, goodChans, goodData, goodICA)
+function save_dataqual_results(filepath, filename, asrStatus, icaStatus, numChans, goodChans, goodData, goodICA, numFrames, numICs)
     res.nChans    = sprintf('%d', numChans);
+    res.nFrames    = sprintf('%d', numFrames);
+    res.nICs    = sprintf('%d', numICs);
     res.goodChans = sprintf('%1.1f', goodChans*100);
+    res.nGoodChans = sprintf('%d', goodChans*numChans);
     res.goodData  = sprintf('%1.1f', goodData*100);
+    res.nGoodData = sprintf('%d', goodData*numFrames);
     res.goodICA   = sprintf('%1.1f', goodICA*100);
+    res.nGoodICs  = sprintf('%d', goodICA*numICs); 
     res.asrFail   = sprintf('%d', asrStatus);
     res.icaFail   = sprintf('%d', icaStatus);
+    res
     fid = fopen(fullfile(filepath, [filename '_dataqual.json'] ), 'w');
     if fid ~= -1
         fprintf(fid, '%s\n', jsonwrite(res));
